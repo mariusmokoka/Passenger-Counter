b62b8611c36a9339ff0a116c955eb3178781828b
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-02-08 10:53:13 +0200

    80%

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-// grab the count-el element, store it in a countEl variable
-let count = 0
 let countEl = document.getElementById("count-el")
-console.log(countEl)
+let count = 0
 
 function increment() {
     count = count + 1
-    // set countEl's innerText to the count
-    countEl.innerText = count 
+    countEl.innerText = count
+}
+
+// 1. Create a function, save(), which logs out the count when it's called
+
+function save() {
+    console.log(count)
 }
 
 
